d0e7de657d021c20ee58f4842455f0317a06582c
Marco Neumann <marco@crepererum.net>
2016-08-12 00:52:25 +0200

    extend tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,23 +1,14 @@
 extern crate rand;
 extern crate rdx_sort;
 
+use std::iter;
+use std::ops;
+
 use rand::{Rng, XorShiftRng};
 use rand::distributions::{IndependentSample, Range};
 use rand::distributions::range::SampleRange;
 
 use rdx_sort::*;
-
-fn generate_data<T>(n: usize, r: &Range<T>, rng: &mut XorShiftRng) -> Vec<T>
-    where T: SampleRange
-{
-    let mut data = Vec::with_capacity(n);
-
-    for _ in (0 as usize)..n {
-        data.push(r.ind_sample(rng));
-    }
-
-    return data;
-}
 
 fn is_sorted<T>(data: &Vec<T>) -> bool
     where T: Clone,
@@ -38,12 +29,28 @@ fn is_sorted<T>(data: &Vec<T>) -> bool
     return true;
 }
 
-fn test_generic<T>(vmin: T, vmax: T, vspecial: Vec<T>)
+fn test_generic<T>(data: Vec<T>)
+    where T: Clone,
+          T: PartialOrd,
+          Vec<T>: RdxSort
+{
+    let mut data = data;
+    let n = data.len();
+    let mut data_sorted_ref = data.clone();
+    data_sorted_ref.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+    data.rdxsort();
+    assert!(data.len() == n, "sorted data has wrong lenght!");
+    assert!(is_sorted(&data), "data is not sorted!");
+    for (x, y) in data.iter().zip(data_sorted_ref.iter()) {
+        assert!(x == y, "sortd data does not match the reference!");
+    }
+}
+
+fn test_rnd_generic<T>(vmin: T, vmax: T, vspecial: Vec<T>)
     where T: Clone,
           T: PartialOrd,
           T: SampleRange,
-          T: Send,
-          T: 'static,
           Vec<T>: RdxSort
 {
     // config
@@ -52,7 +59,10 @@ fn test_generic<T>(vmin: T, vmax: T, vspecial: Vec<T>)
     // generate data
     let r = Range::new(vmin.clone(), vmax.clone());
     let mut rng = XorShiftRng::new_unseeded();
-    let mut data = generate_data(n, &r, &mut rng);
+    let mut data = Vec::with_capacity(n);
+    for _ in (0 as usize)..n {
+        data.push(r.ind_sample(&mut rng));
+    }
     let mut positions = Vec::with_capacity(n);
     for i in 0..n {
         positions.push(i);
@@ -66,53 +76,83 @@ fn test_generic<T>(vmin: T, vmax: T, vspecial: Vec<T>)
     }
     assert!(data.len() == n, "generated data has wrong length!");
 
-    let mut data_sorted_ref = data.clone();
-    data_sorted_ref.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    test_generic(data);
+}
 
-    data.rdxsort();
-    assert!(data.len() == n, "sorted data has wrong lenght!");
-    assert!(is_sorted(&data), "data is not sorted!");
-    for (x, y) in data.iter().zip(data_sorted_ref.iter()) {
-        assert!(x == y, "sortd data does not match the reference!");
-    }
+fn test_full_generic<T>(vmin: T, vmax: T)
+    where T: Clone,
+          T: PartialOrd,
+          T: ops::Add,
+            ops::Range<T> : iter::Iterator,
+        Vec<T>: iter::FromIterator<<ops::Range<T> as iter::Iterator>::Item>,
+          Vec<T>: RdxSort
+{
+    // TODO: use inclusive range once it's stable
+    let mut data: Vec<T> = (vmin.clone()..vmax.clone()).collect();
+    data.push(vmax);
+
+    let mut rng = XorShiftRng::new_unseeded();
+    rng.shuffle(&mut data[..]);
+
+    test_generic(data);
 }
 
 #[test]
-fn test_i8() {
-    test_generic::<i8>(i8::min_value(), i8::max_value(), vec![i8::min_value() + 1, -1i8, 0i8, 1i8, i8::max_value() - 1]);
+fn test_rnd_i8() {
+    test_rnd_generic::<i8>(i8::min_value(), i8::max_value(), vec![i8::min_value() + 1, -1i8, 0i8, 1i8, i8::max_value() - 1]);
 }
 
 #[test]
-fn test_i16() {
-    test_generic::<i16>(i16::min_value(), i16::max_value(), vec![i16::min_value() + 1, -1i16, 0i16, 1i16, i16::max_value() - 1]);
+fn test_full_i8() {
+    test_full_generic::<i8>(i8::min_value(), i8::max_value());
 }
 
 #[test]
-fn test_i32() {
-    test_generic::<i32>(i32::min_value(), i32::max_value(), vec![i32::min_value() + 1, -1i32, 0i32, 1i32, i32::max_value() - 1]);
+fn test_rnd_i16() {
+    test_rnd_generic::<i16>(i16::min_value(), i16::max_value(), vec![i16::min_value() + 1, -1i16, 0i16, 1i16, i16::max_value() - 1]);
 }
 
 #[test]
-fn test_i64() {
-    test_generic::<i64>(i64::min_value(), i64::max_value(), vec![i64::min_value() + 1, -1i64, 0i64, 1i64, i64::max_value() - 1]);
+fn test_full_i16() {
+    test_full_generic::<i16>(i16::min_value(), i16::max_value());
 }
 
 #[test]
-fn test_u8() {
-    test_generic::<u8>(u8::min_value(), u8::max_value(), vec![0u8, 1u8, u8::max_value() - 1]);
+fn test_rnd_i32() {
+    test_rnd_generic::<i32>(i32::min_value(), i32::max_value(), vec![i32::min_value() + 1, -1i32, 0i32, 1i32, i32::max_value() - 1]);
 }
 
 #[test]
-fn test_u16() {
-    test_generic::<u16>(u16::min_value(), u16::max_value(), vec![0u16, 1u16, u16::max_value() - 1]);
+fn test_rnd_i64() {
+    test_rnd_generic::<i64>(i64::min_value(), i64::max_value(), vec![i64::min_value() + 1, -1i64, 0i64, 1i64, i64::max_value() - 1]);
 }
 
 #[test]
-fn test_u32() {
-    test_generic::<u32>(u32::min_value(), u32::max_value(), vec![0u32, 1u32, u32::max_value() - 1]);
+fn test_rnd_u8() {
+    test_rnd_generic::<u8>(u8::min_value(), u8::max_value(), vec![0u8, 1u8, u8::max_value() - 1]);
 }
 
 #[test]
-fn test_u64() {
-    test_generic::<u64>(u64::min_value(), u64::max_value(), vec![0u64, 1u64, u64::max_value() - 1]);
+fn test_full_u8() {
+    test_full_generic::<u8>(u8::min_value(), u8::max_value());
+}
+
+#[test]
+fn test_rnd_u16() {
+    test_rnd_generic::<u16>(u16::min_value(), u16::max_value(), vec![0u16, 1u16, u16::max_value() - 1]);
+}
+
+#[test]
+fn test_full_u16() {
+    test_full_generic::<u16>(u16::min_value(), u16::max_value());
+}
+
+#[test]
+fn test_rnd_u32() {
+    test_rnd_generic::<u32>(u32::min_value(), u32::max_value(), vec![0u32, 1u32, u32::max_value() - 1]);
+}
+
+#[test]
+fn test_rnd_u64() {
+    test_rnd_generic::<u64>(u64::min_value(), u64::max_value(), vec![0u64, 1u64, u64::max_value() - 1]);
 }
