98a05735371497cf44dcf3580e5951739d76611b
Marco Neumann <marco@crepererum.net>
2016-08-12 21:42:37 +0200

    more benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -14,13 +14,14 @@ mod unstable {
 
     use rdxsort::*;
 
-    fn bench_generic<T, F>(b: &mut Bencher, vmin: T, vmax: T, f: F)
+    static N_SMALL:  usize = 1_000;
+    static N_MEDIUM: usize = 10_000;
+    static N_LARGE:  usize = 100_000;
+
+    fn bench_generic<T, F>(b: &mut Bencher, vmin: T, vmax: T, f: F, n: usize)
         where T: Clone + PartialOrd + SampleRange,
               F: Fn(Vec<T>)
     {
-        // config
-        let n = 10_000;
-
         // generate data
         let r = Range::new(vmin.clone(), vmax.clone());
         let mut rng = XorShiftRng::new_unseeded();
@@ -36,123 +37,154 @@ mod unstable {
         });
     }
 
-    fn bench_std_generic<T>(b: &mut Bencher, vmin: T, vmax: T)
+    fn bench_std_generic<T>(b: &mut Bencher, vmin: T, vmax: T, n: usize)
         where T: Clone + PartialOrd + SampleRange,
               Vec<T>: RdxSort
     {
         bench_generic(b, vmin, vmax, |data| {
             let mut data = data;
             data.sort_by(|a, b| a.partial_cmp(b).unwrap());
-        });
+        }, n);
     }
 
-    fn bench_rdxsort_generic<T>(b: &mut Bencher, vmin: T, vmax: T)
+    fn bench_rdxsort_generic<T>(b: &mut Bencher, vmin: T, vmax: T, n: usize)
         where T: Clone + PartialOrd + SampleRange,
               Vec<T>: RdxSort
     {
         bench_generic(b, vmin, vmax, |data| {
             let mut data = data;
             data.rdxsort();
-        });
+        }, n);
     }
 
-    #[bench]
-    fn bench_i8_std(b: &mut Bencher) {
-        bench_std_generic::<i8>(b, i8::min_value(), i8::max_value());
+    macro_rules! bench_type {
+        ($t:ty, $min:expr, $max:expr, [
+            $fn_small_rdxsort:ident,
+            $fn_small_std:ident,
+            $fn_medium_rdxsort:ident,
+            $fn_medium_std:ident,
+            $fn_large_rdxsort:ident,
+            $fn_large_std:ident
+        ]) => {
+            #[bench]
+            fn $fn_small_rdxsort(b: &mut Bencher) {
+                bench_rdxsort_generic::<$t>(b, $min, $max, N_SMALL);
+            }
+
+            #[bench]
+            fn $fn_small_std(b: &mut Bencher) {
+                bench_std_generic::<$t>(b, $min, $max, N_SMALL);
+            }
+
+            #[bench]
+            fn $fn_medium_rdxsort(b: &mut Bencher) {
+                bench_rdxsort_generic::<$t>(b, $min, $max, N_MEDIUM);
+            }
+
+            #[bench]
+            fn $fn_medium_std(b: &mut Bencher) {
+                bench_std_generic::<$t>(b, $min, $max, N_MEDIUM);
+            }
+
+            #[bench]
+            fn $fn_large_rdxsort(b: &mut Bencher) {
+                bench_rdxsort_generic::<$t>(b, $min, $max, N_LARGE);
+            }
+
+            #[bench]
+            fn $fn_large_std(b: &mut Bencher) {
+                bench_std_generic::<$t>(b, $min, $max, N_LARGE);
+            }
+        };
     }
 
-    #[bench]
-    fn bench_i8_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<i8>(b, i8::min_value(), i8::max_value());
-    }
+    bench_type!(f32, 0f32, 1f32, [
+        bench_small_f32_rdxsort,
+        bench_small_f32_std,
+        bench_medium_f32_rdxsort,
+        bench_medium_f32_std,
+        bench_large_f32_rdxsort,
+        bench_large_f32_std
+    ]);
 
-    #[bench]
-    fn bench_i16_std(b: &mut Bencher) {
-        bench_std_generic::<i16>(b, i16::min_value(), i16::max_value());
-    }
+    bench_type!(f64, 0f64, 1f64, [
+        bench_small_f64_rdxsort,
+        bench_small_f64_std,
+        bench_medium_f64_rdxsort,
+        bench_medium_f64_std,
+        bench_large_f64_rdxsort,
+        bench_large_f64_std
+    ]);
 
-    #[bench]
-    fn bench_i16_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<i16>(b, i16::min_value(), i16::max_value());
-    }
+    bench_type!(i8, i8::min_value(), i8::max_value(), [
+        bench_small_i8_rdxsort,
+        bench_small_i8_std,
+        bench_medium_i8_rdxsort,
+        bench_medium_i8_std,
+        bench_large_i8_rdxsort,
+        bench_large_i8_std
+    ]);
 
-    #[bench]
-    fn bench_i32_std(b: &mut Bencher) {
-        bench_std_generic::<i32>(b, i32::min_value(), i32::max_value());
-    }
+    bench_type!(i16, i16::min_value(), i16::max_value(), [
+        bench_small_i16_rdxsort,
+        bench_small_i16_std,
+        bench_medium_i16_rdxsort,
+        bench_medium_i16_std,
+        bench_large_i16_rdxsort,
+        bench_large_i16_std
+    ]);
 
-    #[bench]
-    fn bench_i32_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<i32>(b, i32::min_value(), i32::max_value());
-    }
+    bench_type!(i32, i32::min_value(), i32::max_value(), [
+        bench_small_i32_rdxsort,
+        bench_small_i32_std,
+        bench_medium_i32_rdxsort,
+        bench_medium_i32_std,
+        bench_large_i32_rdxsort,
+        bench_large_i32_std
+    ]);
 
-    #[bench]
-    fn bench_i64_std(b: &mut Bencher) {
-        bench_std_generic::<i64>(b, i64::min_value(), i64::max_value());
-    }
+    bench_type!(i64, i64::min_value(), i64::max_value(), [
+        bench_small_i64_rdxsort,
+        bench_small_i64_std,
+        bench_medium_i64_rdxsort,
+        bench_medium_i64_std,
+        bench_large_i64_rdxsort,
+        bench_large_i64_std
+    ]);
 
-    #[bench]
-    fn bench_i64_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<i64>(b, i64::min_value(), i64::max_value());
-    }
+    bench_type!(u8, u8::min_value(), u8::max_value(), [
+        bench_small_u8_rdxsort,
+        bench_small_u8_std,
+        bench_medium_u8_rdxsort,
+        bench_medium_u8_std,
+        bench_large_u8_rdxsort,
+        bench_large_u8_std
+    ]);
 
-    #[bench]
-    fn bench_u8_std(b: &mut Bencher) {
-        bench_std_generic::<u8>(b, u8::min_value(), u8::max_value());
-    }
+    bench_type!(u16, u16::min_value(), u16::max_value(), [
+        bench_small_u16_rdxsort,
+        bench_small_u16_std,
+        bench_medium_u16_rdxsort,
+        bench_medium_u16_std,
+        bench_large_u16_rdxsort,
+        bench_large_u16_std
+    ]);
 
-    #[bench]
-    fn bench_u8_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<u8>(b, u8::min_value(), u8::max_value());
-    }
+    bench_type!(u32, u32::min_value(), u32::max_value(), [
+        bench_small_u32_rdxsort,
+        bench_small_u32_std,
+        bench_medium_u32_rdxsort,
+        bench_medium_u32_std,
+        bench_large_u32_rdxsort,
+        bench_large_u32_std
+    ]);
 
-    #[bench]
-    fn bench_u16_std(b: &mut Bencher) {
-        bench_std_generic::<u16>(b, u16::min_value(), u16::max_value());
-    }
-
-    #[bench]
-    fn bench_u16_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<u16>(b, u16::min_value(), u16::max_value());
-    }
-
-    #[bench]
-    fn bench_u32_std(b: &mut Bencher) {
-        bench_std_generic::<u32>(b, u32::min_value(), u32::max_value());
-    }
-
-    #[bench]
-    fn bench_u32_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<u32>(b, u32::min_value(), u32::max_value());
-    }
-
-    #[bench]
-    fn bench_u64_std(b: &mut Bencher) {
-        bench_std_generic::<u64>(b, u64::min_value(), u64::max_value());
-    }
-
-    #[bench]
-    fn bench_u64_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<u64>(b, u64::min_value(), u64::max_value());
-    }
-
-    #[bench]
-    fn bench_f32_std(b: &mut Bencher) {
-        bench_std_generic::<f32>(b, 0f32, 1f32);
-    }
-
-    #[bench]
-    fn bench_f32_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<f32>(b, 0f32, 1f32);
-    }
-
-    #[bench]
-    fn bench_f64_std(b: &mut Bencher) {
-        bench_std_generic::<f64>(b, 0f64, 1f64);
-    }
-
-    #[bench]
-    fn bench_f64_rdxsort(b: &mut Bencher) {
-        bench_rdxsort_generic::<f64>(b, 0f64, 1f64);
-    }
+    bench_type!(u64, u64::min_value(), u64::max_value(), [
+        bench_small_u64_rdxsort,
+        bench_small_u64_std,
+        bench_medium_u64_rdxsort,
+        bench_medium_u64_std,
+        bench_large_u64_rdxsort,
+        bench_large_u64_std
+    ]);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -44,30 +44,97 @@
 //! Of course the lower runtime complexity of Radix Sort shows its power when sorting certain data
 //! types. The advantage depends on the size and complexity of the type. While short unsigned
 //! integers benifit the most, long types do not show that huge improvements. The following listing
-//! shows the runtime required for sorting 10'000 Elements, once using the standard library sort
-//! and once using Radix Sort:
+//! shows the runtime required for sorting data sets of different sizes, once using the standard
+//! library sort and once using Radix Sort. The data sets are sampled using an uniform
+//! distribution.
+//!
+//! Keep in mind that the results may vary depending on the hardware, compiler version, operating
+//! system version and configuration and the weather.
+//!
+//!
+//! ### Small (1'000 elements)
+//!
+//! For small data sets Radix Sort can be an advantage for data types with up to 32 bits size. For
+//! 64 bits, standard library sorting should be preferred.
 //!
 //! ```text
-//! test unstable::bench_f32_rdxsort ... bench:     253,863 ns/iter (+/- 178,952)
-//! test unstable::bench_f32_std     ... bench:   1,213,105 ns/iter (+/- 383,496)
-//! test unstable::bench_f64_rdxsort ... bench:     450,055 ns/iter (+/- 219,251)
-//! test unstable::bench_f64_std     ... bench:   1,229,838 ns/iter (+/- 23,368)
-//! test unstable::bench_i16_rdxsort ... bench:     157,572 ns/iter (+/- 3,406)
-//! test unstable::bench_i16_std     ... bench:     493,214 ns/iter (+/- 8,438)
-//! test unstable::bench_i32_rdxsort ... bench:     264,850 ns/iter (+/- 17,677)
-//! test unstable::bench_i32_std     ... bench:     494,648 ns/iter (+/- 22,495)
-//! test unstable::bench_i64_rdxsort ... bench:     483,610 ns/iter (+/- 109,888)
-//! test unstable::bench_i64_std     ... bench:     520,562 ns/iter (+/- 163,783)
-//! test unstable::bench_i8_rdxsort  ... bench:     107,368 ns/iter (+/- 22,236)
-//! test unstable::bench_i8_std      ... bench:     668,804 ns/iter (+/- 88,602)
-//! test unstable::bench_u16_rdxsort ... bench:     149,114 ns/iter (+/- 915)
-//! test unstable::bench_u16_std     ... bench:     561,147 ns/iter (+/- 112,822)
-//! test unstable::bench_u32_rdxsort ... bench:     306,105 ns/iter (+/- 26,152)
-//! test unstable::bench_u32_std     ... bench:     550,718 ns/iter (+/- 54,146)
-//! test unstable::bench_u64_rdxsort ... bench:     585,753 ns/iter (+/- 459,075)
-//! test unstable::bench_u64_std     ... bench:     557,929 ns/iter (+/- 104,396)
-//! test unstable::bench_u8_rdxsort  ... bench:      80,124 ns/iter (+/- 4,273)
-//! test unstable::bench_u8_std      ... bench:     712,865 ns/iter (+/- 21,935)
+//! test unstable::bench_small_f32_rdxsort  ... bench:      25,629 ns/iter (+/- 399)
+//! test unstable::bench_small_f32_std      ... bench:      76,913 ns/iter (+/- 670)
+//! test unstable::bench_small_f64_rdxsort  ... bench:      54,663 ns/iter (+/- 1,589)
+//! test unstable::bench_small_f64_std      ... bench:      81,153 ns/iter (+/- 1,325)
+//! test unstable::bench_small_i16_rdxsort  ... bench:      13,173 ns/iter (+/- 119)
+//! test unstable::bench_small_i16_std      ... bench:      32,490 ns/iter (+/- 547)
+//! test unstable::bench_small_i32_rdxsort  ... bench:      25,470 ns/iter (+/- 302)
+//! test unstable::bench_small_i32_std      ... bench:      30,539 ns/iter (+/- 1,021)
+//! test unstable::bench_small_i64_rdxsort  ... bench:      52,444 ns/iter (+/- 3,399)
+//! test unstable::bench_small_i64_std      ... bench:      30,512 ns/iter (+/- 277)
+//! test unstable::bench_small_i8_rdxsort   ... bench:       8,075 ns/iter (+/- 968)
+//! test unstable::bench_small_i8_std       ... bench:      43,994 ns/iter (+/- 1,099)
+//! test unstable::bench_small_u16_rdxsort  ... bench:      16,006 ns/iter (+/- 587)
+//! test unstable::bench_small_u16_std      ... bench:      33,313 ns/iter (+/- 2,342)
+//! test unstable::bench_small_u32_rdxsort  ... bench:      30,923 ns/iter (+/- 1,738)
+//! test unstable::bench_small_u32_std      ... bench:      34,286 ns/iter (+/- 3,338)
+//! test unstable::bench_small_u64_rdxsort  ... bench:      65,384 ns/iter (+/- 7,461)
+//! test unstable::bench_small_u64_std      ... bench:      34,208 ns/iter (+/- 717)
+//! test unstable::bench_small_u8_rdxsort   ... bench:       8,367 ns/iter (+/- 784)
+//! test unstable::bench_small_u8_std       ... bench:      45,695 ns/iter (+/- 1,256)
+//! ```
+//!
+//!
+//! ### Medium (10'000 elements)
+//!
+//! For medium data sets Radix Sort could be blindly used for all data types since the disadvantage
+//! for types with 64 bits is quite small.
+//!
+//! ```text
+//! test unstable::bench_medium_f32_rdxsort ... bench:     219,547 ns/iter (+/- 5,166)
+//! test unstable::bench_medium_f32_std     ... bench:   1,126,436 ns/iter (+/- 13,894)
+//! test unstable::bench_medium_f64_rdxsort ... bench:     426,906 ns/iter (+/- 7,456)
+//! test unstable::bench_medium_f64_std     ... bench:   1,140,845 ns/iter (+/- 22,528)
+//! test unstable::bench_medium_i16_rdxsort ... bench:     148,963 ns/iter (+/- 2,241)
+//! test unstable::bench_medium_i16_std     ... bench:     470,194 ns/iter (+/- 9,461)
+//! test unstable::bench_medium_i32_rdxsort ... bench:     234,601 ns/iter (+/- 2,066)
+//! test unstable::bench_medium_i32_std     ... bench:     458,349 ns/iter (+/- 7,521)
+//! test unstable::bench_medium_i64_rdxsort ... bench:     455,488 ns/iter (+/- 14,868)
+//! test unstable::bench_medium_i64_std     ... bench:     466,938 ns/iter (+/- 37,052)
+//! test unstable::bench_medium_i8_rdxsort  ... bench:      99,520 ns/iter (+/- 2,304)
+//! test unstable::bench_medium_i8_std      ... bench:     623,255 ns/iter (+/- 10,640)
+//! test unstable::bench_medium_u16_rdxsort ... bench:     141,764 ns/iter (+/- 2,476)
+//! test unstable::bench_medium_u16_std     ... bench:     507,617 ns/iter (+/- 161,140)
+//! test unstable::bench_medium_u32_rdxsort ... bench:     290,928 ns/iter (+/- 4,812)
+//! test unstable::bench_medium_u32_std     ... bench:     508,403 ns/iter (+/- 8,650)
+//! test unstable::bench_medium_u64_rdxsort ... bench:     578,533 ns/iter (+/- 12,939)
+//! test unstable::bench_medium_u64_std     ... bench:     517,811 ns/iter (+/- 42,321)
+//! test unstable::bench_medium_u8_rdxsort  ... bench:      75,994 ns/iter (+/- 3,254)
+//! test unstable::bench_medium_u8_std      ... bench:     661,781 ns/iter (+/- 11,298)
+//! ```
+//!
+//!
+//! ### Large (100'000 elements)
+//!
+//! For large data sets, Radix Sort is great for all data types.
+//!
+//! ```text
+//! test unstable::bench_large_f32_rdxsort  ... bench:   3,140,773 ns/iter (+/- 239,846)
+//! test unstable::bench_large_f32_std      ... bench:  14,467,364 ns/iter (+/- 969,117)
+//! test unstable::bench_large_f64_rdxsort  ... bench:   7,087,326 ns/iter (+/- 592,293)
+//! test unstable::bench_large_f64_std      ... bench:  15,311,056 ns/iter (+/- 421,909)
+//! test unstable::bench_large_i16_rdxsort  ... bench:   1,469,987 ns/iter (+/- 49,132)
+//! test unstable::bench_large_i16_std      ... bench:   5,698,794 ns/iter (+/- 418,976)
+//! test unstable::bench_large_i32_rdxsort  ... bench:   2,767,764 ns/iter (+/- 103,383)
+//! test unstable::bench_large_i32_std      ... bench:   5,890,482 ns/iter (+/- 464,185)
+//! test unstable::bench_large_i64_rdxsort  ... bench:   5,547,276 ns/iter (+/- 262,864)
+//! test unstable::bench_large_i64_std      ... bench:   6,448,590 ns/iter (+/- 78,947)
+//! test unstable::bench_large_i8_rdxsort   ... bench:   1,018,863 ns/iter (+/- 10,951)
+//! test unstable::bench_large_i8_std       ... bench:   7,264,974 ns/iter (+/- 641,619)
+//! test unstable::bench_large_u16_rdxsort  ... bench:   1,530,227 ns/iter (+/- 101,171)
+//! test unstable::bench_large_u16_std      ... bench:   6,185,277 ns/iter (+/- 62,926)
+//! test unstable::bench_large_u32_rdxsort  ... bench:   3,231,873 ns/iter (+/- 284,360)
+//! test unstable::bench_large_u32_std      ... bench:   6,603,391 ns/iter (+/- 80,595)
+//! test unstable::bench_large_u64_rdxsort  ... bench:   6,655,903 ns/iter (+/- 407,888)
+//! test unstable::bench_large_u64_std      ... bench:   7,131,352 ns/iter (+/- 206,972)
+//! test unstable::bench_large_u8_rdxsort   ... bench:     757,607 ns/iter (+/- 63,312)
+//! test unstable::bench_large_u8_std       ... bench:   7,818,080 ns/iter (+/- 109,047)
 //! ```
 //!
 //!
