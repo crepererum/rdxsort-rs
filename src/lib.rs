632ab548a60e869c0dca66ae7d31115a33be56d0
Marco Neumann <marco@crepererum.net>
2016-08-12 12:32:18 +0200

    more docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,90 @@
+//! # RdxSort
+//!
+//! This crate implements [Radix Sort](https://en.wikipedia.org/wiki/Radix_sort) for slices of
+//! different data types, either directly or by exploiting the implementation of other data types.
+//!
+//!
+//! ## Supported Data Types
+//!
+//! Currently, the following data types are supported:
+//!
+//! - **unsigned integers:** native implementation
+//! - **signed integers:** splitting into positive and negative parts and using the unsigned
+//!   implementation
+//! - **floats:** splits data into `-∞`, `(-∞,-0)`, `-0`, `+0`, `(+0,+∞)`, `+∞` and treating the two
+//!   ranges as unsigned integer values. [Subnormals](https://en.wikipedia.org/wiki/Denormal_number)
+//!   and `NaN`s are not supported!
+//!
+//!
+//! ## Example
+//!
+//! ```
+//! use rdx_sort::*;
+//!
+//! fn main() {
+//!     let mut data = vec![2, 10, 0, 1];
+//!     data.rdxsort();
+//!     assert!(data == vec![0, 1, 2, 10]);
+//! }
+//! ```
+//!
+//! ## Implementing New Types
+//!
+//! This crate enables you to add support for new types by implementing `RdxSortTemplate`. It
+//! describes how data is sorted into buckets and how many rounds of sorting are scheduled.
+//!
+//! ```
+//! use rdx_sort::*;
+//!
+//! // `Clone` is required for `RdxSort`
+//! // `PartialEq` is only required for the equality assert, not for the actual sorting
+//! #[derive(Clone, PartialEq)]
+//! struct Foo {
+//!     a: u8,
+//!     b: u8,
+//! }
+//!
+//! impl RdxSortTemplate for Foo {
+//!     fn cfg_nbuckets() -> usize {
+//!         // usually too high, but works as a simple demonstration
+//!         // `256 = 2^8`
+//!         256
+//!     }
+//!
+//!     fn cfg_nrounds() -> usize {
+//!         // one per sub-type
+//!         2
+//!     }
+//!
+//!     fn get_bucket(&self, round: usize) -> usize {
+//!         // return the least significant digit first
+//!         if round == 0 {
+//!             self.b as usize
+//!         } else {
+//!             self.a as usize
+//!         }
+//!     }
+//! }
+//!
+//! fn main() {
+//!     let mut data = vec![
+//!         Foo{a: 5, b: 2},
+//!         Foo{a: 0, b: 1},
+//!         Foo{a: 5, b: 1},
+//!         Foo{a: 0, b: 2},
+//!     ];
+//!     data.rdxsort();
+//!
+//!     let reference = vec![
+//!         Foo{a: 0, b: 1},
+//!         Foo{a: 0, b: 2},
+//!         Foo{a: 5, b: 1},
+//!         Foo{a: 5, b: 2},
+//!     ];
+//!     assert!(data == reference);
+//! }
+//! ```
+
 extern crate core;
 
 /// Radix Sort implementation for some type
