72ae787105a51d40566c6e4f36c60c7ed15ea2bb
Marco Neumann <marco@crepererum.net>
2016-08-18 21:16:26 +0200

    use template for signed integers

--- src/signed_integer.rs
+++ src/signed_integer.rs
@@ -1,93 +1,33 @@
-use super::RdxSort;
+use template::RdxSortTemplate;
 
-use core::ptr;
-
+use std::cmp;
 use std::mem;
-use std::ops;
-
-trait HelperTwosComplementTemplate {
-    type Alias;
-
-    fn min() -> Self;
-    fn zero() -> Self;
-}
-
-trait HelperTwosComplement {
-    fn rdxsort_(&mut self);
-}
-
-// TODO: directly implement `RdxSort` once https://github.com/rust-lang/rfcs/issues/1053 is solved
-impl<T> HelperTwosComplement for [T]
-    where T: HelperTwosComplementTemplate + Clone + ops::Neg + PartialEq + PartialOrd,
-          T::Alias: Clone,
-          <T as ops::Neg>::Output: Into<T>,
-          Vec<T::Alias>: RdxSort
-{
-    fn rdxsort_(&mut self) {
-        assert_eq!(mem::size_of::<T>(), mem::size_of::<T::Alias>());
-
-        let n = self.len();
-        let mut positive: Vec<T::Alias> = Vec::with_capacity(n);
-        let mut negative: Vec<T::Alias> = Vec::with_capacity(n);
-        let mut min: Vec<T> = Vec::new();
-
-        for x in self.iter().cloned() {
-            if x == T::min() {
-                // we cannot invert this value later
-                min.push(x);
-            } else if x >= T::zero() {
-                unsafe {
-                    positive.push(mem::transmute_copy::<T, T::Alias>(&x));
-                }
-            } else {
-                unsafe {
-                    negative.push(mem::transmute_copy::<T, T::Alias>(&((x).into())));
-                }
-            }
-        }
-
-        positive.rdxsort();
-        negative.rdxsort();
-
-        assert!(min.len() <= self.len(), "bug: oversized bucket");
-        unsafe {
-            ptr::copy_nonoverlapping(min.as_ptr(), self.get_unchecked_mut(0), min.len());
-        }
-        let mut pos = min.len();
-        assert!(pos + negative.len() <= self.len(), "bug: oversized bucket");
-        unsafe {
-            ptr::copy_nonoverlapping(negative.as_ptr() as *mut T,
-                                     self.get_unchecked_mut(pos),
-                                     negative.len());
-        }
-        pos += negative.len();
-        assert!(pos + positive.len() <= self.len(),
-                "bug: bucket sizes doe not sum up");
-        unsafe {
-            ptr::copy_nonoverlapping(positive.as_ptr() as *mut T,
-                                     self.get_unchecked_mut(pos),
-                                     positive.len());
-        }
-    }
-}
 
 macro_rules! impl_rdxsort {
     ($t:ty, $alias:ty, $min:expr, $zero:expr) => {
-        impl HelperTwosComplementTemplate for $t {
-            type Alias = $alias;
-
-            fn min() -> Self {
-                $min
+        impl RdxSortTemplate for $t {
+            fn cfg_nbuckets() -> usize {
+                cmp::max(<$alias as RdxSortTemplate>::cfg_nbuckets(), 3)
             }
 
-            fn zero() -> Self {
-                $zero
+            fn cfg_nrounds() -> usize {
+                <$alias as RdxSortTemplate>::cfg_nrounds() + 1
             }
-        }
 
-        impl RdxSort for [$t] {
-            fn rdxsort(&mut self) {
-                self.rdxsort_();
+            fn get_bucket(&self, round: usize) -> usize {
+                if round < <$alias as RdxSortTemplate>::cfg_nrounds() {
+                    let alias = unsafe { mem::transmute::<$t, $alias>(*self) };
+                    alias.get_bucket(round)
+                } else {
+                    if *self == $min {
+                        0
+                    } else if *self >= $zero {
+                        2
+                    } else {
+                        1
+                    }
+
+                }
             }
         }
     }
