db3f57db3a35953ca2c65e3199411191a96014b6
Marco Neumann <marco@crepererum.net>
2016-08-18 21:16:12 +0200

    make char using the u32 implementation

--- src/char.rs
+++ src/char.rs
@@ -1,17 +1,15 @@
 use template::RdxSortTemplate;
 
-// same as u32
 impl RdxSortTemplate for char {
     fn cfg_nbuckets() -> usize {
-        16
+        <u32 as RdxSortTemplate>::cfg_nbuckets()
     }
 
     fn cfg_nrounds() -> usize {
-        8
+        <u32 as RdxSortTemplate>::cfg_nrounds()
     }
 
     fn get_bucket(&self, round: usize) -> usize {
-        let shift = round << 2;
-        (((*self as u32) >> shift) & 15u32) as usize
+        (*self as u32).get_bucket(round)
     }
 }
